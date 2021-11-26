db1ff52ee537f13fe2c1088a66d03b79eb941991
kimnamjun <storypass@naver.com>
2021-11-26 11:31:34 +0900

    model server

--- model_server/my_packages/modeling_one.py
+++ model_server/my_packages/modeling_one.py
@@ -8,6 +8,7 @@ def create_random_forest_model(df: pd.DataFrame) -> RandomForestClassifier:
     :param df: 학습용 데이터입니다.
     :return: 생성된 Random Forest 모델입니다.
     """
+    df.set_index('sha256', inplace=True)
     x, y = df.drop('label', axis=1), df['label']
 
     model = RandomForestClassifier()
