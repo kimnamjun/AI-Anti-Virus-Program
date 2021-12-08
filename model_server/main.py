c84d029a42adf1b60a987909fb5e72d316dca6d8
kimnamjun <storypass@naver.com>
2021-12-08 10:48:40 +0900

    분할

--- model_server/main.py
+++ model_server/main.py
@@ -1,37 +1,12 @@
 import my
 from datetime import datetime
 
-try:
-    print('모델 생성 시작', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print('모델 생성 시작', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-    train_filenames = [f'train_features_{i}.jsonl' for i in range(6)]
-    test_filenames = ['test_features.jsonl']
+train_filenames = [f'train_features_{i}.jsonl' for i in range(6)]
+test_filenames = ['test_features.jsonl']
 
-    train_df_one = my.preprocessing_one.convert_json_to_df(train_filenames)
-    test_df_one = my.preprocessing_one.convert_json_to_df(test_filenames)
-    train_df_one, props_one = my.preprocessing_one.reduce_features_for_train(train_df_one, n_pca=9)
-    test_df_one = my.preprocessing_one.reduce_features_for_test(test_df_one, props_one)
-    model_one = my.model.create_voting_model(train_df_one, test_df_one)
+my.one.create_model(train_filenames, test_filenames)
+my.two.create_model(train_filenames, test_filenames)
 
-    my.aws.save_to_s3(train_df_one, 'ava-data-csv', 'one/train_df.csv')
-    my.aws.save_to_s3(test_df_one, 'ava-data-csv', 'one/test_df.csv')
-    my.aws.save_to_s3(props_one, 'ava-data-model', 'one/properties.pickle')
-    my.aws.save_to_s3(model_one, 'ava-data-model', 'one/voting_model.pickle')
-    print('첫번째 모델 저장 완료', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-
-    train_df_two = my.preprocessing_two.convert_json_to_df(train_filenames)
-    test_df_two = my.preprocessing_two.convert_json_to_df(test_filenames)
-    train_df_two, test_df_two, props_two = my.preprocessing_two.preprocess(train_df_two, test_df_two, max_length=389)
-    model_two = my.model.create_attention_model(train_df_two, test_df_two)
-
-    my.aws.save_to_s3(train_df_two, 'ava-data-csv', 'two/train_df.pickle')
-    my.aws.save_to_s3(test_df_two, 'ava-data-csv', 'two/train_df.pickle')
-    my.aws.save_to_s3(props_two, 'ava-data-model', 'two/properties.pickle')
-    my.aws.save_weight_to_s3('./checkpoint/', 'ava-data-model', 'two/checkpoint/')
-    print('두번째 모델 저장 완료', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-
-except:
-    print('모델 생성 실패', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-
-else:
-    print('모델 생성 종료', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print('모델 생성 종료', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
---
+++ model_server/main_two.py
@@ -0,0 +1,30 @@
+import my
+from datetime import datetime
+
+
+def create_model(train_filenames, test_filenames):
+    try:
+        print('두번째 모델 생성 시작', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+        train_df_two = my.preprocessing_two.convert_json_to_df(train_filenames)
+        test_df_two = my.preprocessing_two.convert_json_to_df(test_filenames)
+        train_df_two, test_df_two, props_two = my.preprocessing_two.preprocess(train_df_two, test_df_two, max_length=389)
+        model_two = my.model.create_attention_model(train_df_two, test_df_two)
+
+        my.aws.save_to_s3(train_df_two, 'ava-data-csv', 'two/train_df.pickle')
+        my.aws.save_to_s3(test_df_two, 'ava-data-csv', 'two/train_df.pickle')
+        my.aws.save_to_s3(props_two, 'ava-data-model', 'two/properties.pickle')
+        my.aws.save_weight_to_s3('./checkpoint/', 'ava-data-model', 'two/checkpoint/')
+
+    except Exception as err:
+        print('두번째 모델 생성 실패', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        raise err
+
+    else:
+        print('두번째 모델 저장 완료', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+
+if __name__ == '__main__':
+    train_filenames = [f'train_features_{i}.jsonl' for i in range(6)]
+    test_filenames = ['test_features.jsonl']
+    create_model(train_filenames, test_filenames)
