9077d9c41071942cebbb60f12152b78c93c02eb9
kimnamjun <storypass@naver.com>
2021-12-09 12:33:17 +0900

    변수 수정 및 main 조정

--- web_server/main.py
+++ web_server/main.py
@@ -1,51 +1,59 @@
-import pickle
 import my
 import os
+import pickle
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from waitress import serve
 from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+props_one = my.aws.load_from_s3('one/properties.pickle', 'ava-data-model')
+model_one = my.aws.load_from_s3('one/model.pickle', 'ava-data-model')
+props_two = my.aws.load_from_s3('two/properties.pickle', 'ava-data-model')
+dataset_two = my.aws.load_from_s3('two/dataset.pickle', 'ava-data-model')
+model_two = my.model.create_my_model(dataset_two)
+model_two = my.aws.load_weights_from_s3(model_two, 'two/checkpoint/', 'ava-data-model')
 
-props = my.aws.load_from_s3('one/properties.pickle', 'ava-data-model')
-model = my.aws.load_from_s3('one/model.pickle', 'ava-data-model')
 
 @app.route('/')
 def index():
-    return render_template('main.html')
-
-@app.route('/Home')
-def Home():
     return render_template('/main.html')
 
-@app.route('/Demonstration')
-def Demonstration():
+
+@app.route('/home')
+def goto_home():
+    return redirect('index')
+
+
+@app.route('/demonstration')
+def goto_demonstration():
     return render_template('/unify-main/home/home-default.html')
 
-@app.route('/Profiles')
-def Profiles():
+
+@app.route('/profiles')
+def goto_profiles():
     return render_template('/unify-main/pages/page-profile-users-2.html')
 
+
 @app.route('/FAQ')
-def FAQ():
+def goto_FAQ():
     return render_template('/unify-main/pages/page-faq-2.html')
 
-@app.route('/Contacts')
-def Contacts():
+
+@app.route('/contacts')
+def goto_contacts():
     return render_template('/unify-main/pages/page-contacts-2.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # setting
         tm = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        # props = my.aws.load_from_s3('one/properties.pickle', 'ava-data-model')
-        # model = my.aws.load_from_s3('one/model.pickle', 'ava-data-model')
 
         # file upload
         file = request.files['file']
+        print(type(file))
         filename = secure_filename(file.filename)
         filename_path = os.path.join('./dataset/temp', filename)
         file.save(os.path.join(filename_path))
@@ -54,39 +62,57 @@ def predict():
         json_file = my.file2pe.convert_file_to_pe(filename_path)
         if not json_file:
             return render_template('/error', error_code=1, error_msg='파일 변환 실패')
-        temp_file_name1 = './dataset/temp/temp.json'
-        with open(temp_file_name1, 'w') as file:
+        json_file_name = './dataset/temp/temp.json'
+        with open(json_file_name, 'w') as file:
             file.write(json_file)
 
-        # json to df
-        df = my.preprocessing_one.convert_json_to_df(temp_file_name1)
-        df = my.preprocessing_one.reduce_features(df, props)
-        df = df.set_index('sha256')
-        df.to_csv('./dataset/temp/pca_df.csv', header=True)  # header=True 맞나여?
+        df1 = my.preprocessing_one.convert_json_to_df(json_file_name)
+        df1 = my.preprocessing_one.reduce_features(df1, props_one)
+        df1.to_csv('./dataset/temp/df_one.csv', index=False)
 
-        # predict
-        x, y = df.drop('label', axis=1), df['label']
-        result = model.predict(x)[0]
+        x1 = df1.drop(['sha256', 'label'], axis=1)
+        result1 = my.model.predict_one(x1, model_one)
+
+        df2 = my.preprocessing_two.preprocess(json_file_name, props_two)
+        with open('./dataset/temp/df_two.pickle', 'wb') as file:
+            pickle.dump(df2, file)
+
+        x2 = df2.drop(['sha256', 'label'], axis=1)
+        result2 = my.model.predict_two(x2, model_two)
 
         # save to aws
         my.aws.save_to_s3('./dataset/temp/temp.json', 'ava-data-json', f'{filename}_{tm}.json')
-        my.aws.save_to_dynamo('./dataset/temp/pca_df.csv', 'AVA-01')
+        my.aws.save_to_dynamo('./dataset/temp/df_one.csv', 'AVA-01')
+        my.aws.save_to_dynamo('./dataset/temp/df_two.pickle', 'AVA-01')
+
+        result = result1 + result2
 
     except Exception as err:
         raise err
     finally:
         path = './dataset/temp/'
-        # for filename in os.listdir(path):
-        #     os.remove(path + filename)
+        for filename in os.listdir(path):
+            os.remove(path + filename)
 
-    return render_template('result.html', result=result)
+    return redirect(url_for('result.html', result=result), code=307)
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    result1 = request.form['result1']
+    result2 = request.form['result2']
+    return render_template('result.html', result1=result1, result2=result2)
 
 
 @app.errorhandler(404)
 def error(error):
     return render_template('/error.html', error_code=404, error_msg='찾을 수 없는 페이지'), 404
 
+
+@app.errorhandler(405)
+def error(error):
+    return render_template('/error.html', error_code=405, error_msg='허용되지 않은 요청'), 405
+
+
+print('Flask app is running')
 serve(app, host='0.0.0.0', port='5021')
-
-
-
