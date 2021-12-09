c4e15c9333792308cdd6a6f1329bed2efb049c81
kimnamjun <storypass@naver.com>
2021-12-09 14:49:22 +0900

    show s3 file list

---
+++ web_server/show_s3_file_list.py
@@ -0,0 +1,10 @@
+import boto3
+
+s3 = boto3.client('s3')
+
+buckets = ['ava-data-model', 'ava-data-json', 'ava-data-csv']
+for bucket in buckets:
+    obj_list = s3.list_objects(Bucket=bucket)
+    contents_list = obj_list['Contents']
+    for content in contents_list:
+        print(bucket, content)
