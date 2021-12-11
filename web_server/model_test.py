69dff4711135a7681775c639eabb1a8ad80cf015
kimnamjun <storypass@naver.com>
2021-12-11 11:24:29 +0900

    bucket name 수정

--- web_server/model_test.py
+++ web_server/model_test.py
@@ -2,14 +2,14 @@ import my
 import numpy as np
 import tensorflow as tf
 
-train_df_two = my.aws.load_from_s3('two/train_df.pickle', 'ava-data-model')
+train_df_two = my.aws.load_from_s3('two/train_df.pickle', 'ava-data-csv')
 x_train = train_df_two['imports'].apply(lambda row: ' '.join(row)).to_list()
 y_train = np.array(train_df_two['label'], dtype='float32')
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(512)
 model_two = my.model.create_my_model(train_dataset)
 model_two = my.aws.load_weights_from_s3(model_two, 'ava-data-model')
 
-test_df_two = my.aws.load_from_s3('two/test_df.pickle', 'ava-data-model')
+test_df_two = my.aws.load_from_s3('two/test_df.pickle', 'ava-data-csv')
 x_test = test_df_two['imports'].apply(lambda row: ' '.join(row)).to_list()
 y_test = np.array(test_df_two['label'], dtype='float32')
 test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(512)
